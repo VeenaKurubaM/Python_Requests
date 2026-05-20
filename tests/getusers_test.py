5e29f0f2dd2b9654a507208f210aec76de159521
Veena Kuruba <javavk2022@gmail.com>
2026-05-20 18:07:46 +1000

    Updated all files

--- tests/getusers_test.py
+++ tests/getusers_test.py
@@ -3,10 +3,12 @@ import pytest
 
 
 @pytest.fixture(scope='module')
-def apis():
+def api_get():
     return APIS()
 
-def test_get_user_validation(apis):
-    response=apis.get("users")
+def test_get_user_validation(api_get):
+    response=api_get.get("users")
     print(response.json())
     assert response.status_code==200
+    print(response.json())
+    assert len(response.json())>0
